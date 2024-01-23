1094850ca265ed2881ae1904f7a4d0ea6d224b7b
ThomasCode92 <thomascode92@hotmail.com>
2024-01-23 07:43:52 +0100

    Sending Files

--- 02-express-api/controllers/messages.controller.js
+++ 02-express-api/controllers/messages.controller.js
@@ -1,5 +1,6 @@
 function getMessages(req, res) {
-  res.send('<ul><li>Hello Albert!</li></ul>');
+  // res.send('<ul><li>Hello Albert!</li></ul>');
+  res.sendFile('skimountain.jpg', { root: './public' });
 }
 
 function postMessages(req, res) {
