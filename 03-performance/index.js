99a53b277b16ca549f305e6969c626154f8f41c3
ThomasCode92 <thomascode92@hotmail.com>
2024-02-02 20:02:05 +0100

    Using PM2 to Create Clusters

--- 03-performance/index.js
+++ 03-performance/index.js
@@ -1,11 +1,6 @@
-const cluster = require('cluster');
-const os = require('os');
-
 const express = require('express');
 
 const app = express();
-
-const NUM_WORKERS = os.cpus().length;
 
 function delay(duration) {
   const start = Date.now();
@@ -23,17 +18,6 @@ app.get('/timer', (req, res) => {
   res.send(`Ding ding ding! - ${process.pid}`);
 });
 
-if (cluster.isMaster) {
-  console.log('Master process has been started');
-  console.log(`Creating ${NUM_WORKERS} worker processes...`);
-
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log(`Worker process has been started on ${process.pid}`);
-
-  app.listen(3000, () => {
-    console.log('Server listening on port: ', 3000);
-  });
-}
+app.listen(3000, () => {
+  console.log('Server listening on port: ', 3000);
+});
