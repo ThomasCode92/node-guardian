280e081796f775da029f694917729d01a6fcb324
ThomasCode92 <thomascode92@hotmail.com>
2024-02-03 11:39:23 +0100

    Worker Threads in Action

---
+++ 03-performance/threads.js
@@ -0,0 +1,10 @@
+const { isMainThread, Worker, workerData } = require('worker_threads');
+
+if (isMainThread) {
+  console.log(`Main thread! Process ID: ${process.pid}`);
+  new Worker(__filename, { workerData: [7, 6, 2, 3] });
+  new Worker(__filename, { workerData: [1, 3, 4, 3] });
+} else {
+  console.log(`Worker thread! Process ID: ${process.pid}`);
+  console.log(`${workerData} sorted is ${workerData.sort()}`);
+}
