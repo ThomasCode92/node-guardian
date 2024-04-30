181b193b863907092d5ad00efc34cf4f3e7cef49
Thomas <thomascode92@hotmail.com>
2024-04-30 19:41:51 +0200

    Modularizing Large GraphQL Projects - Resolvers

--- 05-graphql/index.js
+++ 05-graphql/index.js
@@ -5,19 +5,12 @@ const { createYoga } = require('graphql-yoga');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { loadFilesSync } = require('@graphql-tools/load-files');
 
-const productsModel = require('./products/products.model');
-const ordersModel = require('./orders/orders.model');
-
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
+const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
 
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
-  resolvers: {
-    Query: {
-      products: () => productsModel,
-      orders: () => ordersModel,
-    },
-  },
+  resolvers: resolversArray,
 });
 
 const app = express();
--- 05-graphql/products/products.model.js
+++ 05-graphql/products/products.model.js
@@ -1,4 +1,10 @@
-module.exports = [
+const products = [
   { id: 'redshoe', description: 'Red Shoe', price: 42.12 },
   { id: 'bluejeans', description: 'Blue Jeans', price: 55.55 },
 ];
+
+function getAllProducts() {
+  return products;
+}
+
+module.exports = { getAllProducts };
