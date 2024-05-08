6510ececff192c5bf253f778f824b7a18a9605c3
Thomas <thomascode92@hotmail.com>
2024-05-08 08:17:06 +0200

    Using 'socket.io' with Express

--- 06-websockets/index.js
+++ 06-websockets/index.js
@@ -1,37 +1,16 @@
 const http = require('http');
 const { Server } = require('socket.io');
 
-const server = http.createServer();
-const io = new Server(server, { cors: { origin: '*' } });
+const sockets = require('./sockets');
+const { app } = require('./app');
 
 const PORT = 3000;
-let readyPlayerCount = 0;
 
-server.listen(PORT, () => {
+const httpServer = http.createServer(app);
+const socketServer = new Server(httpServer, { cors: { origin: '*' } });
+
+httpServer.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-io.on('connection', socket => {
-  console.log(`Client with id ${socket.id} connected`);
-
-  socket.on('ready', () => {
-    console.log(`Client with id ${socket.id} is ready`);
-    readyPlayerCount++;
-
-    if (readyPlayerCount % 2 === 0) {
-      io.emit('startGame', socket.id);
-    }
-  });
-
-  socket.on('paddleMove', paddleData => {
-    socket.broadcast.emit('paddleMove', paddleData);
-  });
-
-  socket.on('ballMove', ballData => {
-    socket.broadcast.emit('ballMove', ballData);
-  });
-
-  socket.on('disconnect', reason => {
-    console.log(`Client with id ${socket.id} disconnected: ${reason}`);
-  });
-});
+sockets.listen(socketServer);
